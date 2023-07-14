cb335f87422964263f147db9e95045807fbc892b
Jason Harris <lord.jtharris@gmail.com>
2023-07-14 11:28:02 -0500

    Added category model and starting repo

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tabloid.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a valid category name between 3 and 50 characters.")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
