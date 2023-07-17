5c1ec4a1dfd9915d166de4c75dea824a2cdb442a
Jason Harris <lord.jtharris@gmail.com>
2023-07-17 13:55:01 -0500

    Added short length check and the submit works now.

--- Tabloid/client/src/components/CategoryEditForm.js
+++ Tabloid/client/src/components/CategoryEditForm.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { getCategoryById } from "../modules/categoryManager";
-import { useParams } from "react-router-dom";
+import { getCategoryById, updateCategory } from "../modules/categoryManager";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CategoryEditForm = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
 
     const [category, setCategory] = useState({ id: id, name: "" });
@@ -12,6 +13,7 @@ const CategoryEditForm = () => {
     }, []);
 
     return <>
+        <h2>Edit Category</h2>
         <input type="text" defaultValue={category.name} placeholder="Category Name..." onChange={
             e => {
                 const tempCat = { ...category };
@@ -19,6 +21,18 @@ const CategoryEditForm = () => {
                 setCategory(tempCat);
             }
         } />
+        <button type="submit" onClick={e => {
+            e.preventDefault();
+            if(category.name.length >= 3 && category.name.length <= 50)
+            {
+                updateCategory(id, category);
+                navigate("/categories")
+            }
+            else
+            {
+                window.alert("Name must be between 3 and 50 characters, inclusive.")
+            }
+        }}>Save Changes</button>
     </>
 }
 export default CategoryEditForm;
