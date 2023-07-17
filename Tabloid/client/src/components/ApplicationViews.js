3433aff9fdf4c3753cf4b353653068a8b609aa57
Javier Castillo <javierjcastillo@hotmail.com>
2023-07-17 10:30:16 -0500

    Added View All Post

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Hello from "./Hello";
+import Post from "./Post";
 
 export default function ApplicationViews({ isLoggedIn }) {
   return (
@@ -15,6 +16,7 @@ export default function ApplicationViews({ isLoggedIn }) {
           />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="posts" element={<Post />} />
           <Route path="*" element={<p>Whoops, nothing here...</p>} />
         </Route>
       </Routes>
